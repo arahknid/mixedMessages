7334a87261798984dac48c4fc1744cb657f80f16
arahknid <jb13090@my.bristol.ac.uk>
2022-03-18 00:33:23 +0000

    Minor code cleanup and additions to arrays and text blocks. One bug still to squash.

--- heroGenerator.js
+++ heroGenerator.js
@@ -11,11 +11,11 @@ function elf() {
 };
 
 //// MAIN - races
-const textRace = ['elemental', 'human', elf(), 'orc', 'dwarf', 'lizardman', 'alien', 'goblin', 'gnome', 'treant', 'halfling', 'tiefling', 'minotaur', 'birdperson', 'troll', 'dragonborn', 'fey'];
+const textRace = ['alien', 'birdperson', 'dragonborn', 'dwarf', 'elemental', elf(), 'fey', 'gnome', 'goblin', 'halfling', 'human', 'lizardman', 'minotaur', 'orc', 'tiefling', 'treant', 'troll'];
 
 //  25% chance of having an additional archetype
 function archetype() {
-    const arch = [' werewolf', ' vampire', ' zombie', ' cyborg', ' ghost'];
+    const arch = [' cyborg', ' ghost', ' vampire', ' werewolf', ' zombie'];
     if (Math.random() <= 0.25) {
         return arch[Math.floor(Math.random() * arch.length)]
     } else {
@@ -24,35 +24,44 @@ function archetype() {
 };
 
 //// MAIN - classes
-const textNormalClass = ['warrior', 'fighter', 'ranger', 'wizard', 'sorceror', 'rogue', 'paladin', 'knight', 'crusader', 'bard', 'druid', 'monk', 'barbarian', 'warlock', 'cleric', 'priest', 'shaman', 'samurai', 'ninja', 'thief', 'hunter', 'secret agent', 'pit fighter', 'sniper', 'illusionist', 'soldier', 'spellsword', 'artificer', 'scientist', 'gunslinger', 'loremaster', 'scout', 'warmage', 'summoner', 'swashbuckler', 'gladiator', 'pirate'];
+const textNormalClass = ['artificer', 'barbarian', 'bard', 'cleric', 'crusader', 'druid', 'fighter', 'gladiator', 'gunslinger', 'hunter', 'illusionist', 'knight', 'loremaster', 'monk', 'ninja', 'paladin', 'pirate', 'pit fighter', 'priest', 'ranger', 'rogue', 'samurai', 'scout', 'secret agent', 'shaman', 'sniper', 'soldier', 'sorceror', 'spellsword', 'summoner', 'swashbuckler', 'thief', 'warlock', 'warmage', 'warrior', 'wizard'];
 
-const textDarkClass = ['necromancer', 'serial killer', 'bounty hunter', 'gang enforcer', 'slavedriver', 'assassin', 'human trafficker', 'torturer', 'hangman', 'executioner', 'death knight', 'pyromaniac', 'warlord', 'cultist'];
+const textDarkClass = ['arsonist', 'assassin', 'axe murderer', 'bounty hunter', 'cultist', 'death knight', 'executioner', 'gang enforcer', 'hangman', 'human trafficker', 'necromancer', 'serial killer', 'slavedriver', 'torturer', 'warlord'];
 
-const textMehClass = ['pacifist', 'farmer', 'peasant', 'beggar', 'town guard', 'accountant', 'social media influencer', 'generally disappointing failure', 'business-person', 'hermit'];
+const textMehClass = ['accountant', 'beggar', 'farm hand', 'generally disappointing failure', 'hermit', 'pacifist', 'peasant', 'unsuccessful shopkeeper', 'social media influencer', 'inept town guard'];
 
 
 //// MAIN - generator
 
 function generate() {    
+    // Switch turns on and activates the insertion of a randomised element type, if one of the races is elemental
+    let elementalSwitch = false;
+    
     const randRace = () => {
+        let firstRace = textRace[Math.floor(Math.random() * textRace.length)];
+        let secondRace = textRace[Math.floor(Math.random() * textRace.length)];
+
         // 33.3% chance that the race is a random dual-race 
         if (Math.random() <= (1 / 3)) {
-            let firstRace = textRace[Math.floor(Math.random() * textRace.length)];
-            let secondRace = textRace[Math.floor(Math.random() * textRace.length)];
             // Ensures a dual-race isn't the same race duplicated
             while (secondRace === firstRace) {
                 secondRace = textRace[Math.floor(Math.random() * textRace.length)];
             };
             return `${firstRace}-${secondRace}`;
         } else {
-            return textRace[Math.floor(Math.random() * textRace.length)];
+            return firstRace;
         }
+
+        if (firstRace === textRace[4] || secondRace === textRace) {
+            elementalSwitch = true
+        };
     };
 
     // Extra, randomised description to be inserted IF 'elemental' is one of the options selected.
     function textElemental() {
+        
         const plane = ['Fire', 'Water', 'Earth', 'Wind', 'Thunder and Lightning', 'Darkness', 'Light'];
-        if (randRace().includes('elemental')) {
+        if (elementalSwitch === true) {
             return ` of the Elemental Plane of ${plane[Math.floor(Math.random() * plane.length)]}`
         } else {
             return '';
@@ -69,13 +78,17 @@ function generate() {
             return `You spit on the title of "hero", for you have something truly dark at your core. Did the past murder of your best friend or lover embitter you against all the world? Was your village razed to the ground by raiders, leaving all you held dear in smouldering ruin? Or perhaps your parents just didn't hug you enough. Whatever the cause, you're now the baddest of bad eggs, the most rotten of rotten apples, and the blackest of black sheep. Your title, fearfully whispered in your wake, is...  ${textDarkClass[Math.floor(Math.random() * textDarkClass.length)]}.`    
         // 67% chance of heroic, "normal" class
         } else {
-            return `You are a true hero. Wherever loot lies unlooted, dragons sleep unslain, or damsels remain very much still in distress, you'll be there. You follow the path of the ${textNormalClass[Math.floor(Math.random() * textNormalClass.length)]}.`
+            return `You are a true hero. Wherever loot lies unlooted, dragons sleep unslain, or damsels remain very much still in distress, you'll be there. With burning ambition, indomitable courage, and steel-edged determination, you follow the path... of the ${textNormalClass[Math.floor(Math.random() * textNormalClass.length)]}.`
         }
     };
 
-    // Output
-   console.log(`You are a proud ${randRace() + archetype() + textElemental()}.`);
-   console.log(randClass());
+    // Output... with a 1% chance of apotheosis
+    if (Math.random <= 0.01) {
+        console.log('You are literally God (or one of them, perhaps). You win. Congratulations.')
+    } else {
+        console.log(`You are a proud ${randRace() + archetype() + textElemental()}.`);
+        console.log(randClass());
+    };
 };
 
 // Push the button...
